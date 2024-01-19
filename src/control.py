bcf70c31d490d993a371f4c17b583fc43326f824
yisusfan <156906882+yisusfan@users.noreply.github.com>
2024-01-19 14:15:39 +0100

    Update control.py

--- src/control.py
+++ src/control.py
@@ -7,6 +7,10 @@ milista = lj("src/csv/vgsales.csv")
 
 
 def control(action):
+    """
+    Función de control:
+         Recibe inputs desde el menú y se encarga de llamar a la función correspondiente
+         """
 
     if action == 1:
         milista.add_game()
@@ -15,9 +19,15 @@ def control(action):
         milista.read_list()
 
     elif action == 3:
+        """
+        Imprime los géneros registrados en la base
+        """
         select_genre = milista.genero()
         for i in select_genre:
             print(i, end=" ")
+        """
+        Función filtrar por género
+        """
         genre = input("\nSelecciona un genero para filtrar: ")
         milista.filter_genre(genre)
 
