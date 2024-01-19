4192c5ea2205962aa8c1aa271613a45571857e73
AlbertoEstebanCela <alberto.estebancela@gmail.com>
2024-01-19 08:45:43 +0100

    Actualizacion add_game
    Mejor tiempo de respuesta al comprobar el rank

--- src/lista_juegos.py
+++ src/lista_juegos.py
@@ -11,7 +11,8 @@ class Lista_Juegos:
         Args:
             csv_path (str): string de la ruta del csv.
         """
-        self.lista_csv, self.lista_rank = Lista_Juegos.convert_csv_list(csv_path)
+        self.lista_csv, self.lista_rank = Lista_Juegos.convert_csv_list(
+            csv_path)
 
     @staticmethod
     def convert_csv_list(csv_path):
@@ -56,43 +57,19 @@ class Lista_Juegos:
             if genero == juego.genre:
                 print(juego)
 
-    def load_existing_ranks():
-        #Cambiar para no volver a leer los rangos
-        # Construir la ruta completa al archivo CSV
-        #csv_file_path = 'C:\\Users\\carma\\Visual Studio Code\\Proyecto\\Proyecto-Luca\\src\\csv\\vgsales.csv'
-        script_directory = os.path.dirname(os.path.abspath(__file__))
-        os.chdir(script_directory)
-        
-        csv_folder = 'csv'  # Ruta relativa desde el directorio actual del script
-        csv_file_name = 'vgsales.csv'
-        csv_file_path = os.path.join(csv_folder, csv_file_name)
-
-        # Función para cargar la lista de rangos desde un archivo CSV
-        existing_ranks = []
-        with open(csv_file_path, 'r', newline='') as file:
-            reader = csv.reader(file)
-            next(reader)
-            for row in reader:
-                try:
-                    rank = int(row[0])  
-                    existing_ranks.append(rank)
-                except ValueError:
-                    pass  # Ignoramos las filas que no contienen números en la primera columna
-        return existing_ranks
-
     def add_game(self):
-        #implementar lista rangos
+        # implementar lista rangos
         new_game = Juegos.create_game()
-        Lista_Juegos.load_existing_ranks()
         rank = 1
-        while rank in Lista_Juegos.load_existing_ranks():
-            rank +=1
+
+        while str(rank) in self.lista_rank:
+            rank += 1
+
         new_game.rank = rank
         print(new_game)
         guardar = input("Quieres guardar el juego(Y/N): ")
         if guardar == "Y":
             self.lista_csv.insert(rank-1, new_game)
+            self.lista_rank.insert(rank-1, str(rank))
         else:
             print("Juego no guardado")
-        return self.lista_csv
-
