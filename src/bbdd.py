a06a36829580bc4fce2fbe50caae5d1c4dbbd8f8
yisusfan <156906882+yisusfan@users.noreply.github.com>
2024-01-22 16:04:45 +0100

    Conexion python-mysql

--- src/bbdd.py
+++ src/bbdd.py
@@ -10,8 +10,6 @@ conexion = mysql.connector.connect(user='root',
 def filtro_siglo20():
     # Establece conexion con la base de datos y prepara la query
     cursor = conexion.cursor()
-    nombre = 'railway'
-    cursor.execute("USE {}".format(nombre))
     query = ("SELECT name, platform, year, genre, publisher, na_sales, eu_sales, jp_sales, other_sales, global_sales FROM Juegos "
              "WHERE year < 2001")
 
@@ -19,11 +17,11 @@ def filtro_siglo20():
     cursor.execute(query)
     for (name, platform, year, genre, publisher, na_sales, eu_sales, jp_sales, other_sales, global_sales) in cursor:
         print("Name: {}\n Platform: {}\n Year: {}\n Genre: {}\n Publlisher: {}\n NA_sales: {}\n EU_sales: {}\n JP_sales: {}\n Other_sales: {}\n Global_sales: {}\n".format(name, platform, year, genre, publisher, na_sales, eu_sales, jp_sales, other_sales, global_sales))
+    return "Name: {}\n Platform: {}\n Year: {}\n Genre: {}\n Publlisher: {}\n NA_sales: {}\n EU_sales: {}\n JP_sales: {}\n Other_sales: {}\n Global_sales: {}\n".format(name, platform, year, genre, publisher, na_sales, eu_sales, jp_sales, other_sales, global_sales)
 
     # Cierra la conexion
     cursor.close()
     conexion.close()
-
 
 def listar_editores():
     lista_editores = []
