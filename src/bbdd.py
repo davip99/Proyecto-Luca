770032fed32032180fb795ded49bdde9a6b6567d
AlbertoEstebanCela <alberto.estebancela@gmail.com>
2024-01-22 13:46:51 +0100

    Correcciones bbdd.py

--- src/bbdd.py
+++ src/bbdd.py
@@ -1,37 +1,31 @@
 import mysql.connector
 
 conexion = mysql.connector.connect(user='root',
-    password='FF1CD5HgBggF6Gb3B-eb6452geEHeEE5',
-    host='roundhouse.proxy.rlwy.net',
-    database='railway',
-    port='15049')
-
-cursor = conexion.cursor()
-
-nombre = 'railway'
-cursor.execute("USE {}".format(nombre))
+                                   password='FF1CD5HgBggF6Gb3B-eb6452geEHeEE5',
+                                   host='roundhouse.proxy.rlwy.net',
+                                   database='railway',
+                                   port='15049')
 
 
 def filtro_siglo20(cursor):
+    cursor = conexion.cursor()
     query = ("SELECT name, platform, year, genre, publisher FROM Juegos "
              "WHERE year < 2001")
     cursor.execute(query)
     for (name, platform, year, genre, publisher) in cursor:
-        print("{}, {}, {}, {}, {}".format(name, platform, year, genre, publisher))
+        print("{}, {}, {}, {}, {}".format(
+            name, platform, year, genre, publisher))
 
     cursor.close()
     conexion.close()
 
+
 def listar_editores():
-    lista_editores=[]
+    lista_editores = []
+    cursor = conexion.cursor()
     query = ("SELECT distinct(publisher) FROM `Juegos` ORDER BY publisher;")
     cursor.execute(query)
     for editor in cursor:
         lista_editores.append(editor[0])
     cursor.close()
     return lista_editores
-
-filtro_siglo20(cursor)
-
-
-
