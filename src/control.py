3ecb54e79b4c3b18fb6e101f99176929fc52b476
AlbertoEstebanCela <alberto.estebancela@gmail.com>
2024-01-22 14:17:49 +0100

    Filtrar Nintendo

--- src/control.py
+++ src/control.py
@@ -41,3 +41,9 @@ def control(action):
         lista_editores = bbdd.listar_editores()
         for editor in lista_editores:
             print("- " + editor)
+
+    elif action == 6:
+        # Funcion Listar juegos de Nintendo
+        lista_juegos = bbdd.listar_juegos_nintendo()
+        for juego in lista_juegos:
+            print(juego)
